67f71642adfe66999f1fcc77c2fcf2a6b6e08dc0
Hiroo Ishikawa <hiroo.ishikawa@gmail.com>
2015-08-30 04:20:40 +0900

    Implmeented movements

--- solver.c
+++ solver.c
@@ -3,129 +3,172 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef unsigned long ULONG;
-typedef unsigned int UINT;
-typedef uint64_t U64;
-typedef uint32_t U32;
-typedef uint16_t U16;
-typedef uint8_t U8;
+typedef unsigned long	ULONG;
+typedef unsigned int	UINT;
+typedef uint64_t	U64;
+typedef uint32_t	U32;
+typedef uint16_t	U16;
+typedef uint8_t		U8;
+typedef int64_t		S64;
+typedef int32_t		S32;
+typedef int16_t		S16;
+typedef int8_t		S8;
 
 typedef struct block {
-	UINT x;
-	UINT y;
-	UINT direction;
+	S16 x;
+	S16 y;
+	U8 direction;
+	U16 number;
 } block_t;
 
-static const UINT HORIZONTAL = 0;
-static const UINT VERTICAL = 1;
+typedef struct field {
+	U8 timeout;
+	U16 nblocks;
+	U8 height;
+	U8 width;
+	U8 **grid;
+} field_t;
+
+static const U8 HORIZONTAL = 0;
+static const U8 VERTICAL = 1;
+static const U8 MASK_DIR = VERTICAL;
+static const U8 LEFT2RIGHT = HORIZONTAL | 0x0;
+static const U8 RIGHT2LEFT = HORIZONTAL | 0x2;
+static const U8 TOP2BOTTOM = VERTICAL | 0x0;
+static const U8 BOTTOM2TOP = VERTICAL | 0x2;
+
 static ULONG _rand = 0;
 static UINT _runtime = 0;
-static UINT _timeout = 5;
-static UINT _numbers = 100;
-static UINT _height = 20;
-static UINT _width = 20;
 static UINT _score;
-static UINT _x = 10;
-static UINT _y = 10;
 
-static UINT _field_len;
-static U8 *_field;
-
-static unsigned int
+static U16
 _random(void)
 {
 	const ULONG m = 1UL << 32;
-	const ULONG a = 1103515245;
-	const ULONG c = 12345;
+	const ULONG a = 1103515245UL;
+	const ULONG c = 12345UL;
 	_rand = (a * _rand + c) % m;
-	return (_rand >> 16) & 0x7FFF;
+	return (_rand >> 16) & 0x7FFFUL;
 }
 
 static void
-initialize(int seed)
+initialize(field_t *fp, int seed)
 {
-	_runtime = 0;
+	U8* ptr;
 
-	if (seed) {
-		_rand = seed;
-		_timeout = _random() % 196 + 5;
-		_numbers = _random() % 1901 + 100;
-		_height = _random() % 81 + 20;
-		_width = _random() % 181 + 20;
+	_runtime = 0;
+	_rand = seed;
+	if (_rand) {
+		fp->timeout = _random() % 196 + 5;
+		fp->nblocks = _random() % 1901 + 100;
+		fp->height = _random() % 81 + 20;
+		fp->width = _random() % 181 + 20;
 	}
 	else {
-		_timeout = 5;
-		_numbers = 100;
-		_height = 20;
-		_width = 20;
+		fp->timeout = 5;
+		fp->nblocks = 100;
+		fp->height = 20;
+		fp->width = 20;
 	}
+
 	_score = 0;
-	_y = 10;
-	_x = 10;
-	_field_len = _height * _width;
 
-	_field = malloc(_field_len);
-	memset(_field, 0xFF, _field_len);
-}
-
-#define NG	0
-#define OK	1
-
-#define PROLOGUE(bp)					\
-	block_t tmp;					\
-	memcpy(&tmp, bp, sizeof(block_t));
-
-#define EPILOGUE(bp)					\
-	if (detect_collision(&tmp)) {			\
-		memcpy(bp, &tmp, sizeof(block_t));	\
+	fp->grid = malloc(sizeof(U8*) * fp->width);
+	ptr = malloc(sizeof(U8) * fp->width * fp->height);
+	for (int i = 0; i < fp->width; ++i) {
+		fp->grid[i] = &ptr[i];
+		for (int j = 0; j < fp->height; ++j) {
+			fp->grid[i][j] = 0xF;
+		}
 	}
-
-
-
-static void
-move(UI dir)
-{
 }
 
 static void
-move_down(block_t *b)
+finalize(field_t *fp)
 {
-	PROLOGUE(b);
-	b->y++;
-	EPILOGUE(b);
-}
-
-static void
-move_left(block_t *b)
-{
-	PROLOGUE(b);
-	b->x--;
-	EPILOGUE(b);
-}
-
-static void
-move_right(block_t *b)
-{
-	b->x++;
+	free(fp->grid[0]);
+	free(fp->grid);
 }
 
 static int
-rotate(block_t *b)
+detect_collision(field_t *fp, int x, int y, int c)
 {
-	block_t tmp;
-	memcpy(&tmp, b, sizeof(block_t));
-
-	tmp.x++;
-	tmp.y--;
-	tmp.direction ^= VERTICAL;
-
-	if (detect_collision(&tmp)) {
-		memcpy(b, &tmp, sizeof(block_t));
-		return OK;
+	if (c & MASK_DIR) {	// vertical
+		if (y - 2 >= 0 &&
+		    y < fp->height &&
+		    fp->grid[x][y] > 9 &&
+		    fp->grid[x][y - 1] > 9 &&
+		    fp->grid[x][y - 2] > 9) {
+			return 0;
+		}
 	}
 	else {
-		return NG;
+		if (x >= 0 &&
+		    x + 2 < fp->width &&
+		    fp->grid[x][y] > 9 &&
+		    fp->grid[x + 1][y] > 9 &&
+		    fp->grid[x + 2][y] > 9) {
+			return 0;
+		}
 	}
+	return 1;
+}
+
+static int
+move_down(field_t *fp, block_t *bp)
+{
+	int yy = bp->y + 1;
+	if (!detect_collision(fp, bp->x, yy, bp->direction)) {
+		bp->y = yy;
+		return 1;
+	}
+	return 0;
+}
+
+static int
+move_left(field_t *fp, block_t *bp)
+{
+	int xx = bp->x - 1;
+	if (!detect_collision(fp, xx, bp->y, bp->direction)) {
+		bp->x = xx;
+		return 1;
+	}
+	return 0;
+}
+
+static int
+move_right(field_t *fp, block_t *bp)
+{
+	int xx = bp->x + 1;
+	if (!detect_collision(fp, xx, bp->y, bp->direction)) {
+		bp->x = xx;
+		return 1;
+	}
+	return 0;
+}
+
+static int
+rotate(field_t *fp, block_t *bp)
+{
+	int xx, yy, cc;
+
+	if (bp->direction & MASK_DIR) { // vertical to horizontal
+		xx = bp->x - 1;
+		yy = bp->y - 1;
+	}
+	else {
+		xx = bp->x + 1;
+		yy = bp->y + 1;
+	}
+	cc = (bp->direction + 1) & 0x3;
+
+	if (!detect_collision(fp, xx, yy, cc)) {
+		bp->x = xx;
+		bp->y = yy;
+		bp->direction = cc;
+		return 1;
+	}
+	return 0;
 }
 
 static UINT
@@ -141,6 +184,7 @@ calc_score(ULONG n)
 	return score;
 }
 
+/*
 int
 main(int argc, char *argv[])
 {
@@ -153,3 +197,4 @@ main(int argc, char *argv[])
 
 	return 0;
 }
+*/

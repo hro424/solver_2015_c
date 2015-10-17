4f65326e6d56e7b129b367995cfec32fd3148eef
Hiroo Ishikawa <hiroo.ishikawa@gmail.com>
2015-10-17 20:48:29 +0900

    sorting algorithm

--- c/main.c
+++ c/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <assert.h>
+
+#if defined(DEBUG)
+#define ENTER()		printf("--> %s\n", __func__)
+#define EXIT()		printf("<-- %s\n", __func__)
+#else
+#define ENTER()
+#define EXIT()
+#endif
 
 typedef struct game {
 	FILE *fp;
@@ -8,9 +18,21 @@ typedef struct game {
 	size_t numblocks;
 	size_t height;
 	size_t width;
+	size_t center;
 } game_t;
 
+typedef struct block_t {
+	uint16_t number;
+	uint16_t count;
+	char *ops;
+	size_t ops_len;
+} block_t;
+
 static uint32_t Seed = 0;
+static block_t *BlockBuf;
+static size_t BlockBufLen;
+static block_t **TableBuf;
+static size_t TableBufLen;
 
 static uint16_t
 prand(void)
@@ -18,6 +40,34 @@ prand(void)
 	uint64_t s = Seed;
 	Seed = (s * 1103515245 + 12345) & 0xFFFFFFFF;
 	return (Seed >> 16) & 0x7FFF;
+}
+
+static void
+move_down(block_t *b)
+{
+	assert(b->count < b->ops_len);
+	b->ops[b->count++] = 'D';
+}
+
+static void
+move_left(block_t *b)
+{
+	assert(b->count < b->ops_len);
+	b->ops[b->count++] = 'L';
+}
+
+static void
+move_right(block_t *b)
+{
+	assert(b->count < b->ops_len);
+	b->ops[b->count++] = 'R';
+}
+
+static void
+rotate(block_t *b)
+{
+	assert(b->count < b->ops_len);
+	b->ops[b->count++] = 'C';
 }
 
 static int
@@ -48,6 +98,8 @@ init_game(const char *seed, const char *out, game_t *g)
 		g->width = 20;
 	}
 
+	g->center = g->width / 2;
+
 	return 1;
 }
 
@@ -60,7 +112,7 @@ exit_game(game_t *g)
 }
 
 static void
-play(game_t *g)
+play(const game_t *g)
 {
 	size_t init;
 	size_t hoffset;
@@ -90,13 +142,13 @@ play(game_t *g)
 		fputc('D', g->fp);
 		fputc('C', g->fp);
 
-		if (hoffset > g->width / 2) {
-			for (size_t j = 0; j < hoffset - g->width / 2; ++j) {
+		if (hoffset > g->center) {
+			for (size_t j = 0; j < hoffset - g->center; ++j) {
 				fputc('L', g->fp);
 			}
 		}
 		else {
-			for (size_t j = 0; j < g->width / 2 - hoffset; ++j) {
+			for (size_t j = 0; j < g->center - hoffset; ++j) {
 				fputc('R', g->fp);
 			}
 		}
@@ -109,6 +161,282 @@ play(game_t *g)
 	}
 }
 
+static block_t *
+create_blocks(size_t count, const game_t *g)
+{
+	ENTER();
+
+	block_t *bs;
+	bs = malloc(sizeof(block_t) * count);
+	if (bs == NULL) {
+		fprintf(stderr, "out of memory @ %s\n", __func__);
+		exit(1);
+	}
+
+	for (size_t i = 0; i < count; ++i) {
+		bs[i].ops_len = g->width + g->height + 4;
+		bs[i].ops = malloc(bs[i].ops_len);
+		if (bs[i].ops == NULL) {
+			fprintf(stderr, "out of memmory @ %s\n", __func__);
+			exit(1);
+		}
+	}
+
+	EXIT();
+	return bs;
+}
+
+static void
+destroy_blocks(block_t *bs, size_t count)
+{
+	ENTER();
+
+	if (bs == NULL) {
+		return;
+	}
+
+	for (size_t i = 0; i < count; ++i) {
+		if (bs[i].ops != NULL) {
+			free(bs[i].ops);
+			bs[i].ops = NULL;
+		}
+	}
+
+	free(bs);
+	EXIT();
+}
+
+static block_t *
+get_block_buffer(size_t count, const game_t *g)
+{
+	ENTER();
+
+	block_t *bb;
+
+	if (BlockBuf != NULL && BlockBufLen < count) {
+		destroy_blocks(BlockBuf, BlockBufLen);
+		BlockBuf = NULL;
+	}
+
+	if (BlockBuf == NULL) {
+		BlockBuf = create_blocks(count, g);
+		BlockBufLen = count;
+	}
+
+	bb = BlockBuf;
+
+	for (size_t i = 0; i < count; ++i) {
+		bb[i].number = prand() % 1000;
+		bb[i].count = 0;
+		memset(bb[i].ops, 0, bb[i].ops_len);
+	}
+
+	EXIT();
+	return bb;
+}
+
+
+static block_t **
+create_table(size_t count)
+{
+	ENTER();
+
+	block_t **table;
+
+	table = malloc(sizeof(block_t*) * count);
+	if (table == NULL) {
+		fprintf(stderr, "out of memory @ %s\n", __func__);
+		exit(1);
+	}
+
+	EXIT();
+	return table;
+}
+
+static void
+destroy_table(block_t **t)
+{
+	ENTER();
+
+	if (t != NULL) {
+		free(t);
+	}
+
+	EXIT();
+}
+
+static block_t **
+get_table_buffer(block_t *bs, size_t count)
+{
+	ENTER();
+
+	if (TableBuf != NULL && TableBufLen < count) {
+		destroy_table(TableBuf);
+		TableBuf = NULL;
+	}
+
+	if (TableBuf == NULL) {
+		TableBuf = create_table(count);
+		TableBufLen = count;
+	}
+
+	for (size_t i = 0; i < count; ++i) {
+		TableBuf[i] = &bs[i];
+	}
+
+	EXIT();
+	return TableBuf;
+}
+
+static int
+compare(const void *lhs, const void *rhs)
+{
+	const block_t **l = (const block_t **)lhs;
+	const block_t **r = (const block_t **)rhs;
+
+	return (*l)->number - (*r)->number;
+}
+
+static void
+sort(block_t *bs[], size_t count)
+{
+	qsort(bs, count, sizeof(block_t*), compare);
+}
+
+static void
+play_v(const game_t *g)
+{
+	ENTER();
+
+	block_t *bs;
+	block_t **table;
+
+	bs = get_block_buffer(g->width, g);
+	table = get_table_buffer(bs, g->width);
+
+	sort(table, g->width);
+
+	for (size_t i = 0; i < g->width; ++i) {
+		block_t *bp = table[i];
+		move_down(bp);
+		rotate(bp);
+		if (i < g->center) {
+			for (size_t j = i; j < g->center; ++j) {
+				move_left(bp);
+			}
+		}
+		else {
+			for (size_t j = g->center; j < i; ++j) {
+				move_right(bp);
+			}
+		}
+
+		for (size_t j = 0; j < g->height - 2; ++j) {
+			move_down(bp);
+		}
+	}
+
+	for (size_t i = 0; i < g->width; ++i) {
+		fputs(bs[i].ops, g->fp);
+	}
+
+	EXIT();
+}
+
+static void
+play_h(const game_t *g)
+{
+	ENTER();
+
+	block_t *bs;
+	block_t **table;
+	size_t count = g->width / 3 + g->width % 3;
+
+	bs = get_block_buffer(count, g);
+	table = get_table_buffer(bs, count);
+
+	sort(table, count);
+
+	for (size_t i = 0; i < g->width / 3; ++i) {
+		block_t *bp = table[i];
+		size_t dest = i * 3 + 1;
+
+		if (dest < g->center) {
+			for (size_t j = dest; j < g->center; ++j) {
+				move_left(bp);
+			}
+		}
+		else {
+			for (size_t j = g->center; j < dest; ++j) {
+				move_right(bp);
+			}
+		}
+
+		for (size_t j = 0; j < g->height; ++j) {
+			move_down(bp);
+		}
+	}
+
+	for (size_t i = g->width / 3; i < g->width % 3; ++i) {
+		block_t *bp = table[i];
+		move_down(bp);
+		rotate(bp);
+		if (i < g->center) {
+			for (size_t j = i; j < g->center; ++j) {
+				move_left(bp);
+			}
+		}
+		else {
+			for (size_t j = g->center; j < i; ++j) {
+				move_right(bp);
+			}
+		}
+
+		for (size_t j = 0; j < g->height - 2; ++j) {
+			move_down(bp);
+		}
+	}
+
+	for (size_t i = 0; i < g->width / 3; ++i) {
+		fputs(bs[i].ops, g->fp);
+	}
+
+	EXIT();
+}
+
+static void
+play_horizontal(const game_t *g)
+{
+	play_h(g);
+}
+
+static void
+play_vertical(const game_t *g)
+{
+	size_t n;
+	for (n = 0; n < g->numblocks; n += g->width) {
+		play_v(g);
+	}
+
+	if (n == g->numblocks) {
+		play_v(g);
+	}
+	else {
+		play_h(g);
+	}
+}
+
+static void
+play2(const game_t *g)
+{
+	if (g->numblocks < g->width) {
+		play_horizontal(g);
+	}
+	else {
+		play_vertical(g);
+	}
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -118,12 +446,19 @@ main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
+	BlockBuf = NULL;
+	BlockBufLen = 0;
+	TableBuf = NULL;
+	TableBufLen = 0;
 
 	if (!init_game(argv[1], argv[2], &game)) {
 		return EXIT_FAILURE;
 	}
 
-	play(&game);
+	play2(&game);
+
+	destroy_blocks(BlockBuf, BlockBufLen);
+	destroy_table(TableBuf);
 
 	exit_game(&game);
 
--- rust/solver/eval.sh
+++ rust/solver/eval.sh
@@ -5,7 +5,7 @@ count=1000
 SOLVER=./target/release/solver
 
 for i in `seq 0 $count`; do
-  score=`./collatzris.py $i $SOLVER moves.txt 0 | grep score | cut -f2 -d':'`
+  score=`python2 ./collatzris.py $i $SOLVER moves.txt 0 | grep score | cut -f2 -d':'`
   total=`expr $total + $score`
   echo "[$i] score $score total $total"
 done
